8a663796f9319476b9309e13bf7004694cf811f7
Jovian <99047257@mydavinci.nl>
2021-12-20 14:40:08 +0100

    Update readme + opdrachten

--- myfirstwindow.py
+++ myfirstwindow.py
@@ -1,10 +1,3 @@
-# tkinter-101
-## F8.01.O1
-Deze GUI inspireert mij het meest, want het is op windows. Ik ben daarmee ook het meest bekend bij.
-Het ziet er ook gewoon makkelijk uit en de overzicht is ook duidelijk
-## F8.01.O2
-Explosief Venster Gebruik
-``` python
 import tkinter 
 
 window = tkinter.Tk()
@@ -34,4 +27,3 @@ for i in range(7):
 
 
 window.mainloop()
-```
