c3efb38df2b9684428ba0e5859162f8801c2d443
Jovian <99047257@mydavinci.nl>
2021-12-24 17:04:38 +0100

    Update readme

--- README.md
+++ README.md
@@ -86,3 +86,36 @@ block3.pack(
 
 window.mainloop()
 ```
+## F1.8.01.O4
+Een klok
+``` python
+import tkinter
+import time
+
+def timeUpdate():
+    firstnumbers = tkinter.StringVar(value=time.strftime('%H:%M:%S'))
+    clock.set(firstnumbers)
+    label.configure(textvariable=firstnumbers)
+    root.after(200, timeUpdate)
+    
+root = tkinter.Tk()
+root.title("Clock")
+clock = tkinter.StringVar(value=time.strftime('%H:%M:%S'))
+
+label = tkinter.Label(root,
+    font = ("Modern", 200),
+    bg = "black",
+    fg = "red"
+    )
+label.configure(textvariable=clock)
+label.pack(
+    ipadx = 10,
+    ipady = 10,
+    fill = "both",
+    expand = True
+)
+
+timeUpdate()
+
+root.mainloop()
+```
--- klok.py
+++ klok.py
@@ -1,14 +1,29 @@
 import tkinter
+import time
 
-from time import strftime
+def timeUpdate():
+    firstnumbers = tkinter.StringVar(value=time.strftime('%H:%M:%S'))
+    clock.set(firstnumbers)
+    label.configure(textvariable=firstnumbers)
+    root.after(200, timeUpdate)
+    
+root = tkinter.Tk()
+root.title("Clock")
+clock = tkinter.StringVar(value=time.strftime('%H:%M:%S'))
 
-window = tkinter.Tk()
-window.title =("clock")
+label = tkinter.Label(root,
+    font = ("Modern", 200),
+    bg = "black",
+    fg = "red"
+    )
+label.configure(textvariable=clock)
+label.pack(
+    ipadx = 10,
+    ipady = 10,
+    fill = "both",
+    expand = True
+)
 
+timeUpdate()
 
-
-
-
-
-
-window.mainloop
+root.mainloop()
